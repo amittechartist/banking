256062090471cb5b760f4656b0c8a7204761f294
amittechartist <amit.techartist@gmail.com>
2025-07-14 10:19:00 +0530

    feat: create sign in form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -4,8 +4,37 @@ import React, { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+    Form
+} from "@/components/ui/form"
+import CustomInput from './CustomInput'
+import { authFormSchema } from '@/lib/utils'
+import { Loader2 } from 'lucide-react'
+
+
 const AuthForm = ({ type }: { type: string }) => {
     const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof authFormSchema>>({
+        resolver: zodResolver(authFormSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof authFormSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
     return (
         <section className='auth-form'>
             <header className='flex flex-col gap-5 md:gap-8'>
@@ -30,7 +59,29 @@ const AuthForm = ({ type }: { type: string }) => {
             ) : (
                 <>
                     {/* Form */}
-                    
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                            <CustomInput
+                                control={form.control}
+                                name='email'
+                                label="Email"
+                                placeholder="Enter your email"
+                            />
+
+                            <CustomInput
+                                control={form.control}
+                                name='password'
+                                label="Password"
+                                placeholder="Enter your password"
+                            />
+
+                            <Button className='form-btn' type="submit">
+                                {isLoading ? <>
+                                    <Loader2 className='animate-spin' /> &nbsp; Loading...
+                                </> : type === 'sign-in' ? 'Sign In' : 'Sign Up'}
+                            </Button>
+                        </form>
+                    </Form>
                 </>
             )}
         </section>
